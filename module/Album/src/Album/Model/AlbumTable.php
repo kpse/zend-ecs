3d6dd6e1408a0165f0bb22d39ecb7120b51de798
Louis Suo <louis.suo@myob.com>
2017-02-08 15:16:35 +1100

    use stsClient on cloud

--- module/Album/src/Album/Model/AlbumTable.php
+++ module/Album/src/Album/Model/AlbumTable.php
@@ -2,7 +2,9 @@
 
 namespace Album\Model;
 
+use Aws\Sts\StsClient;
 use Aws\DynamoDb\DynamoDbClient;
+
 
 class AlbumTable {
 
@@ -10,9 +12,7 @@ class AlbumTable {
 
 	public function __construct()
 	{
-		$this->client = DynamoDbClient::factory(array(
-			'region'  => 'ap-southeast-2'
-		));
+		$this->client = $this->client();
 	}
 
 	public function fetchAll()
@@ -30,5 +30,33 @@ class AlbumTable {
 		}, iterator_to_array($iterator));
 
 	}
+
+	public function client()
+	{
+		if ($_SERVER['APPLICATION_ENV'] == 'development') {
+			return DynamoDbClient::factory(array(
+				'region'  => 'ap-southeast-2'
+			));
+		}
+		return $this->stsClient();
+
+	}
+
+	public function stsClient() {
+		$sts = StsClient::factory();
+
+		$result = $sts->assumeRole([
+			'RoleArn' => 'arn:aws:iam::226019795248:role/dynamodb_role', // REQUIRED
+			'RoleSessionName' => 'remote_dynamo_client', // REQUIRED
+		]);
+
+		$credentials = $result->get('Credentials');
+		return DynamoDbClient::factory(array(
+			'key'    => $credentials['AccessKeyId'],
+			'secret' => $credentials['SecretAccessKey'],
+			'token'  => $credentials['SessionToken']
+		));
+	}
+
 
 }
